db258eb5d07677e1d05196e40ae124ddc620369c
Werner Klaus <csam9224@student.uibk.ac.at>
2018-04-11 17:48:22 +0200

    Datentypen durchgegangen

--- main.js
+++ main.js
@@ -24,3 +24,35 @@ let isOver18 = true;
 console.log("Sind Sie über 18: ", isOver18);
 let age = 17;
 console.log("age über 18", age > 18);
+
+let zahlenliste = [2, 11, 24, 33];
+console.log(zahlenliste);
+
+let zeichenliste = ["a","b","c","d"];
+console.log(zeichenliste);
+console.log(zeichenliste[0]);
+console.log("Anzahl der Strings: ", zeichenliste.length);
+
+//Zuweisungssyntax beachten!!
+let benutzer = {
+    vorname: "John",
+    nachname: "Doe",
+    alter: 29
+};
+console.log("Benutzer Alter: ", benutzer.alter);
+console.log("Benutzer: ", benutzer);
+
+benutzer.bestzeit = 100;
+console.log("Benutzer Bestzeit: ", benutzer.bestzeit);
+console.log("Benutzer: ", benutzer);
+
+benutzer["bestzeit ever"] = 300;
+console.log("Benutzer: ", benutzer);
+
+let test = {};
+test.test = 2;
+console.log(test.test);
+
+
+//Benutzer Eingabe
+prompt("Wie alt bist du?");
