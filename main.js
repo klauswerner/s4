e2d7b3ddec81d0978912770f3bcbde5f0f682314
Werner Klaus <csam9224@student.uibk.ac.at>
2018-04-11 17:24:59 +0200

    neues JS Projekt erstellt

---
+++ main.js
@@ -0,0 +1,26 @@
+//alert("Hello World");
+
+let nachricht = "Hallo Welt";
+//alert(nachricht);
+nachricht = "neue Welt";
+//alert(nachricht);
+console.log(nachricht);
+const linkColor ="#ff0000";
+let neue_variable="test";
+console.log("Variable: eine_variable: ", neue_variable);
+let highscore = 452.532;
+console.log("Mein Highscore:", highscore/10);
+let stringi = "String1";
+console.log(stringi);
+stringi ="String2";
+console.log(stringi);
+stringi = `Stringi3 
+2te Zeile
+3te Zeile`;
+console.log(stringi);
+let theDude = 'Jeffery "The Dude" Lebowsky';
+console.log(theDude);
+let isOver18 = true;
+console.log("Sind Sie über 18: ", isOver18);
+let age = 17;
+console.log("age über 18", age > 18);
